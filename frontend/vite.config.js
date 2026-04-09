254638b0e5c65a8054cb94075d546f10762824ae
Akshay Panigrahi <akshaypanigrahi7@gmail.com>
2026-04-10 02:35:48 +0530

    Use viteSingleFile to embed all assets into index.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   base: command === 'build' ? '/day-planner-app/' : '/',
 }))
